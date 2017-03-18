b3bf997703fcb5d53e067c36a629552b25f1509d
Leeeeo21 <zhangnu3@qq.com>
2017-03-18 19:54:33 +0800

    V21.0 图片版：增加爆炸图片显示

--- src/Explode.java
+++ src/Explode.java
@@ -10,29 +10,43 @@ public class Explode {
     int[] diameter = {5,10,15,20,25,30,35,27,20,10,5,2};
     int step = 0;
     Color color = new Color(255, 61, 54);
-    TankClientFrame tc = null;
+    private TankClientFrame tc = null;
+
+
+
+    private static Toolkit tk = Toolkit.getDefaultToolkit();      //Toolkit类
+    private static Image[] imgs = { //把Image设成静态，以免每次生成对象都重载图片
+            tk.getImage(Explode.class.getClassLoader().getResource("images/0.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/1.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/2.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/3.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/4.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/5.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/6.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/7.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/8.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/9.gif")),
+            tk.getImage(Explode.class.getClassLoader().getResource("images/10.gif"))
+    };//class（反射），通过Toolkit将classpath下的图片导入程序
 
     Explode(int x,int y,TankClientFrame tc){
         this.x = x;
         this.y = y;
         this.tc = tc;
     }
+
     public void draw(Graphics g){
         if(!this.Live) {
             tc.explodes.remove(this);
             return;
         }
-        if(step == diameter.length){
+        if(step == imgs.length){
             step = 0;
             Live = false;
             return;
         }
-
-        Color c = g.getColor();
-        g.setColor(color);
-        g.fillOval(x,y,diameter[step],diameter[step]);
+        g.drawImage(imgs[step],x,y,null);
         step ++ ;
 
-        g.setColor(c);
     }
 }
